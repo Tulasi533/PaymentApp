64e52a4d4fc781b4fa628b11c659a8f2a0391161
Tulasi533 <18pa1a0533@gmail.com>
2022-08-16 03:05:13 +0000

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { HomeComponent } from './containers/home/home.component';
+import { TransactionComponent } from './containers/transaction/transaction.component';
+import { HistoryComponent } from './containers/history/history.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SenderComponent } from './containers/sender/sender.component';
+import { ReceiverComponent } from './containers/receiver/receiver.component';
+import { TransComponent } from './containers/trans/trans.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'transaction', component: TransactionComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'transaction/sender', component: SenderComponent},
+  { path: 'transaction/receiver', component: ReceiverComponent},
+  { path: 'transaction/trans', component: TransComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    HomeComponent,
+    TransactionComponent,
+    HistoryComponent,
+    SenderComponent,
+    ReceiverComponent,
+    TransComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
